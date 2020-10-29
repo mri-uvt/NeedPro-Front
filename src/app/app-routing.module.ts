4eec2db3030351edef73edf9fdfb5fadbcf6e499
Skander Haouam <44363467+madnaks@users.noreply.github.com>
2020-10-29 23:34:43 +0100

    add routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { FourOhFourComponent } from './core/four-oh-four/four-oh-four.component';
 import { AuthenticationComponent } from './user/authentication/authentication.component';
+import { RegistrationComponent } from './user/registration/registration.component';
 
 const routes: Routes = [
-  { path: '', component: AuthenticationComponent},
-  { path:'not-found', component: FourOhFourComponent},
-  { path:'**', redirectTo: 'not-found'}
+  { path: '', component: RegistrationComponent},
+  { path: 'authentication', component: AuthenticationComponent},
+  { path: 'registration', component: RegistrationComponent},
+  { path: 'not-found', component: FourOhFourComponent},
+  { path: '**', redirectTo: 'not-found'}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
