885009c268dc8762bfbbcdcb9af1b4fab3730d23
Skander Haouam <44363467+madnaks@users.noreply.github.com>
2020-10-29 23:34:35 +0100

    fix app module refrences

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,26 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatRadioModule } from '@angular/material/radio';
+
 import { FourOhFourComponent } from './core/four-oh-four/four-oh-four.component';
 import { HeaderComponent } from './core/header/header.component';
 import { RegistrationComponent } from './user/registration/registration.component';
 import { AuthenticationComponent } from './user/authentication/authentication.component';
+import { FormsModule,ReactiveFormsModule  } from '@angular/forms';
+
+const modules = [
+  MatToolbarModule,
+  MatStepperModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatButtonModule,
+  MatRadioModule
+];
 
 @NgModule({
   declarations: [
@@ -17,13 +33,18 @@ import { AuthenticationComponent } from './user/authentication/authentication.co
     FourOhFourComponent,
     HeaderComponent,
     RegistrationComponent,
-    AuthenticationComponent
+    AuthenticationComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    FormsModule,
+    ReactiveFormsModule,
+    modules
+  ],
+  exports: [
+    modules
   ],
   providers: [],
   bootstrap: [AppComponent]
