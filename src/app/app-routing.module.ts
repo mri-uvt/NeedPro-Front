b7ccddda40d7c526f326e4e05eee48dd9b93c6bb
Skander Haouam <44363467+madnaks@users.noreply.github.com>
2020-10-29 18:21:18 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FourOhFourComponent } from './core/four-oh-four/four-oh-four.component';
+import { AuthenticationComponent } from './user/authentication/authentication.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AuthenticationComponent},
+  { path:'not-found', component: FourOhFourComponent},
+  { path:'**', redirectTo: 'not-found'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FourOhFourComponent } from './core/four-oh-four/four-oh-four.component';
+import { HeaderComponent } from './core/header/header.component';
+import { RegistrationComponent } from './user/registration/registration.component';
+import { AuthenticationComponent } from './user/authentication/authentication.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FourOhFourComponent,
+    HeaderComponent,
+    RegistrationComponent,
+    AuthenticationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
