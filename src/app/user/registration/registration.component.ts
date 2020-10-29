aaab90ca8c63a0ae826cff8bffb5e10da2bb9920
Skander Haouam <44363467+madnaks@users.noreply.github.com>
2020-10-29 23:35:02 +0100

    implement registraiton

--- src/app/user/registration/registration.component.ts
+++ src/app/user/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatStepper } from '@angular/material/stepper';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  userType: UserType;
 
-  ngOnInit(): void {
+  userTypes = UserType;
+
+  constructor(private formBuilder: FormBuilder) {
   }
 
+  ngOnInit() {
+
+    this.firstFormGroup = this.formBuilder.group({
+      pseudo: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      floatLabelControl: ['particular'],
+    });
+
+    this.secondFormGroup = this.formBuilder.group({
+      lastName: ['', Validators.required],
+      firstName: ['', Validators.required],
+      country: [''],
+      city: [''],
+      sexe: [''],
+      experience: [''],
+      specialiy: [''],
+      yearOfExperience: [''],
+      biographie: ['']
+    });
+
+  }
+
+  goForward(stepper: MatStepper) {
+    this.userType = this.firstFormGroup.controls['floatLabelControl'].value;
+    if(this.userType) {
+      stepper.next();
+    }
+  }
+}
+
+export enum UserType {
+  Particular = 'particular',
+  Professional = 'professional'
 }
